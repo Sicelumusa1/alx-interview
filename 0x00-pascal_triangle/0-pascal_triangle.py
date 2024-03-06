ca1eb6b65584dffae9faf8304153fb752e138700
Sicelumusa <musaqwabe@gmail.com>
2024-03-06 00:42:42 -0800

    Fixed pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -13,14 +13,14 @@ def pascal_triangle(n):
     Returns:
         list of lists of integers representing the Pascal’s triangle of n
     """
-    if  n <= 0:
+    if n <= 0:
         return []
 
     triangle = []
 
     for row in range(n):
         current_row = [1]
-        if  row > 0:
+        if row > 0:
             prev_row = triangle[-1]
             for i in range(1, row):
                 current_row.append(prev_row[i - 1] + prev_row[i])
