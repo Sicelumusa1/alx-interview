5645e6a0d0a19e9cbd29fb444151813cce256b08
Sicelumusa <musaqwabe@gmail.com>
2024-03-14 11:08:12 -0700

    added the lockboxes module

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A module that detemines if all boxes in a list of boxes can be unlocked
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    Detemines if all boxes in a list of boxes can be unlocked
+
+    Args:
+        boxes (list[list[int]]): A list of boxes where each box contains
+        a list list of keys
+
+    Returns:
+            bool: True if all boxes can be unlocked, False otherwise.
+    """
+    #  Initialize a set to keep track of boxes that can be unlocked
+    unlocked_boxes = set([0])
+
+    #  Initialize a queue for BFS traversal
+    queue = [0]
+
+    #  BFS traversal
+    while queue:
+        current_box = queue.pop(0)
+        keys = boxes[current_box]
+        for key in keys:
+            if key not in unlocked_boxes:
+                unlocked_boxes.add(key)
+                queue.append(key)
+
+    #  Check if all boxes are unlocked
+    return len(unlocked_boxes) == len(boxes)
