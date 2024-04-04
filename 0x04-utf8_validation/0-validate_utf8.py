388f649d7a4bbd61e0af6e07ea333cdc1a1a70b4
Sicelumusa <musaqwabe@gmail.com>
2024-04-04 09:14:22 +0000

    Added inline comments and No continuation expected edge case

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,10 +4,11 @@
 Determines if a given data set represents a valid UTF-8 encoding
 """
 
+
 def validUTF8(data):
     """
     Determines if a given data set represents a valid UTF-8 encoding
-    
+
     Args:
         data: The data to validate
     """
@@ -16,21 +17,23 @@ def validUTF8(data):
 
     #  Iterate through each integer in the data set
     for byte in data:
+        # Check if the most significant bit is 0
         if num_bytes == 0:
             if byte >> 7 == 0b0:
-                continue
+                continue  # Valid single-byte character
             elif byte >> 5 == 0b110:
-                num_bytes = 1
+                num_bytes = 1  # Expecting 1 more byte for 2 byte char
             elif byte >> 4 == 0b1110:
-                num_bytes = 2
+                num_bytes = 2  # Expecting 2 more byte for 3 byte char
             elif byte >> 3 == 0b11110:
-                num_bytes = 3
+                num_bytes = 3  # Expecting 3 more byte for 4 byte char
             else:
-                return False
+                return False  # Invalid start byte
         else:
+            # Check continuation pattern
             if byte >> 6 != 0b10:
-                return False
+                return False  # Invalid continuation byte
 
-            num_bytes -= 1
-
-    return num_bytes == 0
+            num_bytes -= 1  # Successfully processed a character
+    # Ensure all characters were fully processed
+    return num_bytes == 0 and num_bytes != -1
