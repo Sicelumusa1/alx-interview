8c8d917248deadd19c7bb6ae13744f752414e783
Sicelumusa <musaqwabe@gmail.com>
2024-05-14 12:07:22 -0700

    fixed pycodestyle

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,66 +1,70 @@
 #!/usr/bin/python3
 
-"""A module to determine the winner of the prime game""" 
+"""A module to determine the winner of the prime game"""
+
 
 def isWinner(x, nums):
-  """
-  Determine the winner of a series of games where Maria and Ben take turns removing prime numbers from a set
-
-  Args:
-    x (int): The number of rounds to play
-    nums (list): A list of integers representing the upper bounds of the sets for each round
-  
-  Returns:
-    str or None: the name of he winner or None if it is a tie
-  """
-  def primes_up_to_n(n):
     """
-    Generate all prime numbers up to n
+    Determine the winner of a series of games where Maria and Ben take
+    turns removing prime numbers from a set
 
     Args:
-      n (int): The upper bound for prime number generation
+        x (int): The number of rounds to play
+        nums (list): A list of integers representing the upper bounds of
+        the sets for each round
 
     Returns:
-      list: Alist containing all prime numbers up to n
+        str or None: the name of he winner or None if it is a tie
     """
-    primes = [True] * (n + 1)
-    primes[0], primes[1] = False, False
-    p = 2
-    while p * p <= n:
-      if  primes[p]:
-        for i in range(p * p, n + 1, p):
-          primes[i] = False
-      p += 1
-    return [i for i in range(n + 1) if primes[i]]
-  
-  def can_win(n):
-    """
-    Determine if the player whose turn is to play first can win the game
 
-    Args:
-      n (int): The upper bound for the set in the current game.
-    
-    Returns:
-      bool: True if the starting player can win, False othrwise
-    """
-    primes = primes_up_to_n(n)
-    if len(primes) == 0:
-      return False
-    return len(primes) % 2 == 1
-  
-  maria_wins = 0
-  ben_wins = 0
+    def primes_up_to_n(n):
+        """
+        Generate all prime numbers up to n
 
-  for n in nums:
-    if can_win(n):
-        maria_wins += 1
+        Args:
+            n (int): The upper bound for prime number generation
+
+        Returns:
+            list: Alist containing all prime numbers up to n
+        """
+        primes = [True] * (n + 1)
+        primes[0], primes[1] = False, False
+        p = 2
+        while p * p <= n:
+            if primes[p]:
+                for i in range(p * p, n + 1, p):
+                    primes[i] = False
+            p += 1
+        return [i for i in range(n + 1) if primes[i]]
+
+    def can_win(n):
+        """
+        Determine if the player whose turn is to play first can win the game
+
+        Args:
+            n (int): The upper bound for the set in the current game.
+
+        Returns:
+            bool: True if the starting player can win, False othrwise
+        """
+        primes = primes_up_to_n(n)
+        if len(primes) == 0:
+            return False
+        return len(primes) % 2 == 1
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        if can_win(n):
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    # Determine the overall winner
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
     else:
-      ben_wins += 1
-  
-  # Determine the overall winner
-  if maria_wins > ben_wins:
-    return "Maria"
-  elif ben_wins > maria_wins:
-    return "Ben"
-  else:
-    return None
+        return None
